4498ea4448e760dd4af92350b3973c9f11db5e84
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-06-20 14:33:05 +0200

    WIP

--- pytec/rinex.py
+++ pytec/rinex.py
@@ -1,10 +1,24 @@
 import libtecrs
+import polars as pl
+import time
 
 
 def main():
-    input_path = "./rinex/v2/obs/cgtc0920.14o"
-    output_path = "./output/cgtc0920.parquet"
-    # libtecrs.convert_rinex_to_parquet(input_path, output_path)
+    files = [
+        "./rinex/v2/obs/cgtc0920.14o",
+        "./rinex/v3/obs/ASIR00ITA_R_20242810000_01D_30S_MO.crx",
+    ]
+
+    for file_path in files:
+        print(f"\nProcessing file: {file_path}")
+
+        start_time = time.time()
+        df = libtecrs.read_rinex_obs_to_polars(file_path)
+        load_time = time.time() - start_time
+
+        print(f"Load time: {load_time:.2f} s")
+        print(f"Number of rows: {len(df):,}")
+        print(df.head(5))
 
 
 if __name__ == "__main__":
