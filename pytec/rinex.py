b1c512376ea1e64efb58d2c213d6722def0a9ed8
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-06-24 19:12:16 +0200

    WIP: testing OBS and NAV parsing

--- pytec/rinex.py
+++ pytec/rinex.py
@@ -1,9 +1,8 @@
-from libtecrs import read_rinex_obs_to_polars
-import polars as pl
+from libtecrs import read_rinex_obs, read_rinex_nav
 import time
 
 
-def main():
+def test_obs():
     files = [
         "./rinex/v2/obs/cgtc0920.14o",
         "./rinex/v3/obs/ASIR00ITA_R_20242810000_01D_30S_MO.crx",
@@ -13,7 +12,7 @@ def main():
         print(f"\nProcessing file: {file_path}")
 
         start_time = time.time()
-        df, (x, y, z) = read_rinex_obs_to_polars(file_path)
+        df, (x, y, z) = read_rinex_obs(file_path)
         load_time = time.time() - start_time
 
         print(f"Load time: {load_time:.2f} s")
@@ -27,8 +26,28 @@ def main():
             on="observable",
             aggregate_function="first",
         )
-
         print(df_pivot.head(5))
+
+
+def test_nav():
+    files = [
+        "./rinex/v2/nav/BRDC00GOP_R_20140920000_01D_MN.rnx",
+    ]
+
+    for file_path in files:
+        print(f"\nProcessing file: {file_path}")
+
+        start_time = time.time()
+        nav_by_constellation_dict = read_rinex_nav(file_path)
+        load_time = time.time() - start_time
+
+        print(f"Load time: {load_time:.2f} s")
+        print(nav_by_constellation_dict)
+
+
+def main():
+    test_obs()
+    test_nav()
 
 
 if __name__ == "__main__":
