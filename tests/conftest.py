39ac5d079cb0d6bb7158bcf1742663318d709a98
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-06-25 16:15:25 +0200

    improve OBS file tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,9 @@ def test_data_dir():
     return Path(__file__).parent.parent / "rinex"
 
 
+# Observation File Fixtures
+
+
 @pytest.fixture
 def obs_v2_file(test_data_dir):
     return str(test_data_dir / "v2" / "obs" / "cgtc0920.14o")
@@ -14,7 +17,22 @@ def obs_v2_file(test_data_dir):
 
 @pytest.fixture
 def obs_v3_file(test_data_dir):
+    return str(test_data_dir / "v3" / "obs" / "ASIR00ITA_R_20242810000_01D_30S_MO.rnx")
+
+
+@pytest.fixture
+def obs_v3_hatanaka_compressed_file(test_data_dir):
     return str(test_data_dir / "v3" / "obs" / "ASIR00ITA_R_20242810000_01D_30S_MO.crx")
+
+
+@pytest.fixture
+def obs_v3_gzip_file(test_data_dir):
+    return str(
+        test_data_dir / "v3" / "obs" / "ASIR00ITA_R_20242810000_01D_30S_MO.crx.gz"
+    )
+
+
+# Navigation File Fixtures
 
 
 @pytest.fixture
@@ -22,6 +40,7 @@ def nav_v2_file(test_data_dir):
     return str(test_data_dir / "v2" / "nav" / "BRDC00GOP_R_20140920000_01D_MN.rnx")
 
 
+# Other File Fixtures
 @pytest.fixture
 def invalid_file(tmp_path):
     return str(tmp_path / "nonexistent.obs")
--- tests/test_obs.py
+++ tests/test_obs.py
@@ -4,6 +4,7 @@ import pytest
 
 
 def test_output_types(obs_v2_file):
+    """Check that the output types of read_rinex_obs are correct"""
     df, coords = read_rinex_obs(obs_v2_file)
     assert isinstance(df, DataFrame)
     assert isinstance(coords, tuple)
@@ -12,6 +13,7 @@ def test_output_types(obs_v2_file):
 
 
 def test_read_rinex_obs_valid_v2(obs_v2_file):
+    """Test reading a RINEX v2 observation file"""
     df, (x, y, z) = read_rinex_obs(obs_v2_file)
     assert df.shape[0] > 0
     assert all(col in df.columns for col in ["epoch", "sv", "observable", "value"])
@@ -19,13 +21,29 @@ def test_read_rinex_obs_valid_v2(obs_v2_file):
 
 
 def test_read_rinex_obs_valid_v3(obs_v3_file):
+    """Test reading a RINEX v3 observation file"""
     df, (x, y, z) = read_rinex_obs(obs_v3_file)
     assert df.shape[0] > 0
     assert all(col in df.columns for col in ["epoch", "sv", "observable", "value"])
     assert not all(v is None for v in [x, y, z])
 
 
+def test_read_rinex_obs_valid_v3_hatanaka(obs_v3_hatanaka_compressed_file):
+    """Test reading a (Hatanaka-compressed) RINEX v3 observation file"""
+    df, (x, y, z) = read_rinex_obs(obs_v3_hatanaka_compressed_file)
+    assert df.shape[0] > 0
+    assert all(col in df.columns for col in ["epoch", "sv", "observable", "value"])
+    assert not all(v is None for v in [x, y, z])
+
+
+# def test_read_rinex_obs_valid_v3_gzipped(obs_v3_gzip_file):
+#     df, (x, y, z) = read_rinex_obs(obs_v3_gzip_file)
+#     assert df.shape[0] > 0
+#     assert all(col in df.columns for col in ["epoch", "sv", "observable", "value"])
+#     assert not all(v is None for v in [x, y, z])
+
+
 def test_read_rinex_obs_nonexistent_file(invalid_file):
-    """Check that an error is raised with a non-existent file"""
+    """Check that a FileNotFoundError is raised with a non-existent file"""
     with pytest.raises(FileNotFoundError):
         read_rinex_obs(invalid_file)
