4aac61edab8b708704385e4dbca43f4b56942245
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-07-01 18:47:49 +0200

    add GNSS constants

--- pytec/satellites/__init__.py
+++ pytec/satellites/__init__.py
@@ -73,3 +73,33 @@ EPHEMERIS_FIELDS: dict[str, list[str]] = {
     "QZSS": [],
     "SBAS": [],
 }
+
+GNSS_CONSTANTS = {
+    "GPS": {
+        "gm": 3.986005e14,
+        "we": 7.2921151467e-5,
+        "a": 6378137,
+        "e": 0.0818191908426215,
+        "f": 1 / 298.257223563,
+    },
+    "GLONASS": {
+        "gm": 3.9860044e14,
+        "we": 7.292115e-5,
+        "a": 6378136,
+        "c20": -1082.63e-6,
+    },
+    "Galileo": {"gm": 3.986004418e14, "we": 7.2921151467e-5, "a": 6378137},
+    "BeiDou": {
+        "gm": 3.986004418e14,
+        "we": 7.292115e-5,
+        "a": 6378137,
+        "f": 1 / 298.257222101,
+    },
+}
+# gm: geocentric gravitational constant (m^3*s^-2)
+# we: angular rotation of Earth (rad*s^-1)
+# a: major-axis of Earth ellipsoid (m)
+# e: numeric eccentricity of ellipsoid (-)
+# f: flattening of Earth ellipsoid (-)
+
+TOL_KEPLER = 0.001
