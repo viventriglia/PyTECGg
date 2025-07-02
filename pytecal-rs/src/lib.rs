f1a24d78f8dfe1bd6d431e0bb3e58d1dd113cebe
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-07-02 15:52:01 +0200

    rename from pytecal to pytecal-rs to avoid confusion between Python modules and Rust crates

--- pytecal-rs/src/lib.rs
+++ pytecal-rs/src/lib.rs
@@ -183,7 +183,7 @@ fn read_rinex_nav(path: &str) -> PyResult<BTreeMap<String, PyDataFrame>> {
 }
 
 #[pymodule]
-fn pytecal(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn pytecalrs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_rinex_obs, m)?)?;
     m.add_function(wrap_pyfunction!(read_rinex_nav, m)?)?;
     Ok(())
